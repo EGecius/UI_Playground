0da0a27c9b845a262ead37f24a58e052679341ae
Egidijus Gecius <e.gecius@gmail.com>
2017-10-19 09:14:20 +0100

    tidy up MainActivity

--- app/src/main/java/com/egecius/ui_playground/MainActivity.java
+++ app/src/main/java/com/egecius/ui_playground/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView image2;
-    private ImageView image3;
+    private ImageUtils mUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         image2 = (ImageView) findViewById(R.id.image_2);
-        image3 = (ImageView) findViewById(R.id.image_3);
 
         applyTinting();
     }
 
     private void applyTinting() {
         Context context = getApplicationContext();
-        Drawable drawable = new ImageUtils().toTintedDrawable(context, R.drawable.ic_dog_barking,
+        mUtils = new ImageUtils();
+        Drawable drawable = mUtils.toTintedDrawable(context, R.drawable.ic_dog_barking,
                 android.R.color.holo_orange_light);
         image2.setImageDrawable(drawable);
     }
