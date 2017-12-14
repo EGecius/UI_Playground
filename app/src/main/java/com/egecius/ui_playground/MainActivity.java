c8e249cf00bc0086a1825dcfd992bf9062443ac2
update gradle <>
2017-12-14 11:02:21 +0000

    add detail activity

--- app/src/main/java/com/egecius/ui_playground/MainActivity.java
+++ app/src/main/java/com/egecius/ui_playground/MainActivity.java
@@ -1,9 +1,11 @@
 package com.egecius.ui_playground;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createListener();
         image2 = (ImageView) findViewById(R.id.image_2);
 
         applyTinting();
+    }
+
+    private void createListener() {
+        findViewById(R.id.to_detail_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDetailActivity();
+            }
+        });
+    }
+
+    private void showDetailActivity() {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 
     private void applyTinting() {
