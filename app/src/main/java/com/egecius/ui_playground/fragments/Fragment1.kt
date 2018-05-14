b5a3c62633f0db56cf0982f714e219873f680e33
Egidijus Gecius <>
2018-05-14 16:49:38 +0100

    convet to Kotlin

---
+++ app/src/main/java/com/egecius/ui_playground/fragments/Fragment1.kt
@@ -0,0 +1,26 @@
+package com.egecius.ui_playground.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.egecius.ui_playground.R
+
+class Fragment1 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_1, container, false)
+        setOnClickListener(rootView)
+
+        return rootView
+    }
+
+    private fun setOnClickListener(rootView: View) {
+        rootView.findViewById<View>(R.id.to_fragment_2).setOnClickListener { (activity as MyFragmentActivity).showFragment2() }
+    }
+
+
+}
